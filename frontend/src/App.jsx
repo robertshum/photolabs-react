5aa88e9631a2da2ab6cd8c2dafc4b5f8c0e1c99e
Robert Shum <robertshum@gmail.com>
2023-11-01 01:38:30 -0700

    close modal onClick of the close button.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,7 +14,7 @@ const App = () => {
 
   return (
     <div className="App">
-      {isShowModal && <PhotoDetailsModal />}
+      {isShowModal && <PhotoDetailsModal showModal={showModal} />}
       <HomeRoute showModal={showModal} />
     </div>
   );
