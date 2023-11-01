16feb31d45e34d9f130d2c22c6daf15b1aef49a0
Robert Shum <robertshum@gmail.com>
2023-10-31 20:52:47 -0700

    added components and css for top navigation bar.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,13 +3,13 @@ import PhotoList from './components/PhotoList';
 import './App.scss';
 
 //temp remove when done
-import TopicList from 'components/TopicList';
+import TopNavigationBar from 'components/TopNavigationBar';
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
   return (
     <div className="App">
-      <TopicList />
+      <TopNavigationBar />
     </div>
   );
 };
