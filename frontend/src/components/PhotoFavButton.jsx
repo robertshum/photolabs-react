9edb07cb473a30f272429f053fa97b31448233f3
Robert Shum <robertshum@gmail.com>
2023-10-31 18:37:40 -0700

    added heart item and toggle function.

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -1,13 +1,22 @@
-import React, { useCallback, useState } from 'react';
-
+import React, { useState } from 'react';
 import FavIcon from './FavIcon';
 import '../styles/PhotoFavButton.scss';
 
 function PhotoFavButton() {
+
+  const [isHearted, setIsHearted] = useState(false);
+
+  const handleClick = () => {
+    setIsHearted(!isHearted);
+  };
+
   return (
     <div className="photo-list__fav-icon">
-      <div className="photo-list__fav-icon-svg">
-        {/* Insert React */}
+      <div onClick={handleClick}>
+        <div className="photo-list__fav-icon-svg">
+          <FavIcon displayAlert={false} selected={isHearted} />
+          {/* Insert React */}
+        </div>
       </div>
     </div>
   );
--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,10 +1,12 @@
 import React, { Fragment } from "react";
-
+import PhotoFavButton from "./PhotoFavButton";
 import "../styles/PhotoListItem.scss";
+
 
 const PhotoListItem = (props) => {
   return (
     <div className='photo-list__item'>
+      <PhotoFavButton></PhotoFavButton>
       <img className='photo-list__image' src={props.imageSource}></img>
       <div className='photo-list__user-details'>
         <img className='photo-list__user-profile' src={props.profile}></img>
